2cd6ec429f2b19c69f4ec40c7be45a6d413ea299
Aiker Ary Acosta Cantillo <aikerary@gmail.com>
2024-05-11 12:10:15 -0500

    Testing asyncrhony for bonus

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,28 +1,50 @@
-import React from "react";
-import Fruta from "../../components/fruta/Fruta";
-
+import { useEffect, useState } from "react"
+import Fruta from "../../components/fruta/Fruta"
+ 
 const Cart = () => {
-    const fruta=
-        {
-            id: 14,
-            name: "Persimmon",
-            price: 3.3,
-            vendors: [
-              {
-                id: 3,
-                name: "True Fruits Inc.",
-                self_link: "/shop/v2/vendors/3",
-              },
-            ],
-            image_link: "/shop/v2/products/14/image",
-          };
-  return (
-    <section>
-      <Fruta
-        fruta={fruta}
-      />
-    </section>
-  );
-};
-
+ 
+    const [idFruits, setIdFruits] = useState([])
+    const [fruits, setFruits] = useState([])
+ 
+    const getAllIdFruits = async () => {
+        const response = await fetch(import.meta.env.VITE_FRUIT_API + '/shop/v2/products')
+        const data = await response.json()
+ 
+        setIdFruits(data.products.map(product => product.id))
+    }
+ 
+    const getAllFruits = async () => {
+        const fetchedFruits = await Promise.all(idFruits.map(async id => {
+            const response = await fetch(import.meta.env.VITE_FRUIT_API + '/shop/v2/products/' + id)
+            const data = await response.json()
+ 
+            return data
+        }))
+ 
+        setFruits(fetchedFruits)
+    }
+ 
+    const fetchs = async() => {
+        await Promise.all([getAllIdFruits(), getAllFruits()])
+    }
+ 
+    useEffect(() => {
+        fetchs()
+    }, [])
+ 
+    useEffect(() => {
+        // Update fruits state whenever idFruits changes
+        getAllFruits()
+    }, [idFruits])
+ 
+    return (
+        <section>
+            {/* <Fruta fruta={} /> */}
+            {fruits.map((fruit, index) => (
+                <Fruta key={index} fruta={fruit} />
+            ))}
+        </section>
+    )
+}
+ 
 export default Cart;
