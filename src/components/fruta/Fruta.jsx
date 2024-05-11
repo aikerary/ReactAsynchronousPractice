a4ddb076e5645e27ea00e4dd26cb8a979d4ca999
Aiker Ary Acosta Cantillo <aikerary@gmail.com>
2024-05-11 09:56:21 -0500

    First configs of Fruta

---
+++ src/components/fruta/Fruta.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Fruta = () => {
+  return (
+    <article>
+        <div>
+            <img src="" alt="" />
+        </div>
+        <div>
+            <h2>Name</h2>
+            <p>Price</p>
+            <a href="#">Vendor</a>
+        </div>
+    </article>
+  );
+};
+
+export default Fruta;
